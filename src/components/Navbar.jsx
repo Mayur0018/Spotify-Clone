d42b987e5364ab19e3975a71b3fc3482070b0399
Mayur0018 <mayurnish18@gmail.com>
2025-05-11 14:43:34 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
           </p>
           <p className="bg-black text-[15px] px-3 py-1 rounded-2xl cursor-pointer">Install App</p>
           <p className=" bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center">D</p>
-        </div>
+        </div>    
       </div>
     </>
   );
