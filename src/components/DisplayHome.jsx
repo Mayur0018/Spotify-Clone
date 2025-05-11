e89c3ac5b6a3a71ae239ee355d0b560f66cee087
Mayur0018 <mayurnish18@gmail.com>
2025-05-11 21:14:07 +0530

    commit

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import Navbar from "./Navbar";
+import AllbumIteams from "./AllbumIteams";
+import { albumsData } from "../assets/assets";
 const DisplayHome = () => {
   return (
     <>
       <Navbar />
+      <div className=" mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AllbumIteams
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,9 +20,22 @@ const Navbar = () => {
           <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer">
             Explore Premium
           </p>
-          <p className="bg-black text-[15px] px-3 py-1 rounded-2xl cursor-pointer">Install App</p>
-          <p className=" bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center">D</p>
-        </div>    
+          <p className="bg-black text-[15px] px-3 py-1 rounded-2xl cursor-pointer">
+            Install App
+          </p>
+          <p className=" bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center">
+            D
+          </p>
+        </div>
+      </div>
+      <div className="flex items-center gap-2 mt-4">
+        <p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer">
+          All
+        </p>
+        <p className="bg-black  px-4 py-1 rounded-2xl cursor-pointer">Music</p>
+        <p className="bg-black  px-4 py-1 rounded-2xl cursor-pointer">
+          Podcasts
+        </p>
       </div>
     </>
   );
