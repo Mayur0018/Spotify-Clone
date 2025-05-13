08101c82195a57b18bef24df0f719f522d180ced
Mayur0018 <mayurnish18@gmail.com>
2025-05-13 12:26:37 +0530

    commit

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import DisplayHome from "./DisplayHome";
 import DisplayAlbum from "./DisplayAlbum";
+import { useRef } from "react";
+import { useLocation } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
+import { albumsData } from "../assets/assets";
 const Display = () => {
+  const displyRef = useRef();
+  const location = useLocation();
+  const isAlbum = location.pathname.includes("album");
+  const albumId = isAlbum ? location.pathname.slice(-1) : "";
+  const bgColor = albumsData[Number(albumId).bgColor];
+  console.log(bgColor);
+
   return (
-    <div className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg-[w-75%] lg:ml-0">
+    <div
+      ref={displyRef}
+      className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg-[w-75%] lg:ml-0"
+    >
       <Routes>
         <Route path="/" element={<DisplayHome />}></Route>
         <Route path="/albums/:id" element={<DisplayAlbum />}></Route>
